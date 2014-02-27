0fb463efa233ac320e07712b4904602094e43750
Mark Engel <mark@mbr-targeting.com>
2014-02-27 16:47:23 +0100

    Added sync option (to reset database

---
+++ share/drop-all-tables.sql
@@ -0,0 +1,10 @@
+DO $$
+BEGIN
+    CREATE TABLE IF NOT EXISTS foo ();
+    EXECUTE (
+        SELECT string_agg('DROP TABLE IF EXISTS ' || table_name || ' CASCADE;', '')
+        FROM information_schema.tables
+        WHERE table_schema = 'public' AND table_type = 'BASE TABLE'
+    );
+END
+$$;

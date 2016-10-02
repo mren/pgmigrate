992a2029673d70118b05209c61edd9361c0ffb11
mren <mren@users.noreply.github.com>
2016-10-02 13:50:01 +0200

    Simplify mockfs in tests.

--- test/migrate.js
+++ test/migrate.js
@@ -24,12 +24,12 @@ describe('migrate', () => {
     client.query('DROP TABLE IF EXISTS test, schema_info')
   );
 
+  afterEach(mockFs.restore);
+
   it('should migrate a schema', () => {
     mockFs({
-      path: {
-        '2016-01-01T17:00:00Z-name.sql': 'CREATE TABLE test (value TEXT);',
-        '2016-01-01T19:00:00Z-another-name.sql': 'INSERT INTO test (value) VALUES (\'value\');',
-      },
+      'path/2016-01-01T17:00:00Z-name.sql': 'CREATE TABLE test (value TEXT);',
+      'path/2016-01-01T19:00:00Z-another-name.sql': 'INSERT INTO test (value) VALUES (\'value\');',
     });
     return migrate('path', getConnection)
       .then(() => client.query('SELECT * FROM test'))
@@ -38,40 +38,32 @@ describe('migrate', () => {
       .then(result => assert.deepEqual(result.rows, [
         { version: '2016-01-01T17:00:00Z-name.sql' },
         { version: '2016-01-01T19:00:00Z-another-name.sql' },
-      ]))
-      .then(() => mockFs.restore());
+      ]));
   });
 
   it('should not migrate an invalid schema', () => {
     mockFs({
-      path: {
-        '2016-01-01T17:00:00Z-name.sql': 'create invalid sql',
-      },
+      'path/2016-01-01T17:00:00Z-name.sql': 'create invalid sql',
     });
     return migrate('path', getConnection)
       .then(() => assert(false))
-      .catch((err) => {
-        assert.strictEqual(err.message, 'syntax error at or near "invalid"');
-        mockFs.restore();
-      });
+      .catch(err => assert.strictEqual(err.message, 'syntax error at or near "invalid"'));
   });
 
-
   it('should be idempotent', () => {
-    mockFs({ path: {
-      '20000000000000-name.sql': 'CREATE TABLE test (value TEXT);',
-    } });
+    mockFs({
+      'path/20000000000000-name.sql': 'CREATE TABLE test (value TEXT);',
+    });
     return migrate('path', getConnection)
-      .then(() => migrate('path', getConnection))
-      .then(() => mockFs.restore());
+      .then(() => migrate('path', getConnection));
   });
 
   it('should allow hooks', () => {
-    mockFs({ path: {
-      '0000-00-00T00:00:00:000Z-prehook.sql': 'INSERT INTO test (value) VALUES (\'pre\')',
-      '2016-01-01T00:00:00:000Z-name.sql': 'INSERT INTO test (value) VALUES (\'value\')',
-      '9999-99-99T99:99:99:999Z-posthook.sql': 'INSERT INTO test (value) VALUES (\'post\')',
-    } });
+    mockFs({
+      'path/0000-00-00T00:00:00:000Z-prehook.sql': 'INSERT INTO test (value) VALUES (\'pre\')',
+      'path/2016-01-01T00:00:00:000Z-name.sql': 'INSERT INTO test (value) VALUES (\'value\')',
+      'path/9999-99-99T99:99:99:999Z-posthook.sql': 'INSERT INTO test (value) VALUES (\'post\')',
+    });
     return client.query('CREATE TABLE test (value TEXT)')
       .then(() => migrate('path', getConnection))
       .then(() => migrate('path', getConnection))
@@ -82,36 +74,31 @@ describe('migrate', () => {
         { value: 'post' },
         { value: 'pre' },
         { value: 'post' },
-      ]))
-      .then(() => mockFs.restore());
+      ]));
   });
 
   it('should allow to sync database', () => {
-    mockFs({ path: {
-      '20000000000000-name.sql': 'CREATE TABLE test (value TEXT);',
-    } });
+    mockFs({
+      'path/20000000000000-name.sql': 'CREATE TABLE test (value TEXT);',
+    });
     return migrate('path', getConnection)
       .then(() => client.query('INSERT INTO test (value) VALUES (\'value\')'))
       .then(() => migrate('path', getConnection, true))
       .then(() => client.query('SELECT * FROM test'))
-      .then(result => assert.deepEqual(result.rows, []))
-      .then(() => mockFs.restore());
+      .then(result => assert.deepEqual(result.rows, []));
   });
 
   it('should call done in happy case', () => {
     const done = sinon.stub();
-    mockFs({ path: {
-    } });
+    mockFs({ path: {} });
     const getConnectionStub = cb => cb(null, { query: () => Promise.resolve({ rows: [] }) }, done);
     return migrate('path', getConnectionStub)
-      .then(() => mockFs.restore())
       .then(() => sinon.assert.calledOnce(done));
   });
 
   it('should call done in error case', () => {
     const done = sinon.stub();
-    mockFs({ path: {
-    } });
+    mockFs({ path: {} });
     const getConnectionStub = cb => cb(null, { query: () => Promise.reject(new Error()) }, done);
     return migrate('path', getConnectionStub)
       .then(() => assert(false))

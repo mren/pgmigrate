33c8287ae0eb8086d8d5121d73397d4ad271425c
mren <mren@users.noreply.github.com>
2016-12-22 18:39:12 +0100

    Use pg.Pool.
    - require an instance of pg.Pool for migrate.

--- migrate.js
+++ migrate.js
@@ -1,27 +1,11 @@
+const assert = require('assert');
 const fs = require('fs');
 
 const dropSql = fs.readFileSync(`${__dirname}/share/drop-all-tables.sql`, 'utf8');
 
-function migrate(path, connection, isSync) {
-  if (connection instanceof Function) {
-    return new Promise((resolve, reject) => {
-      connection((err, client, done) => {
-        if (err) {
-          return reject(err);
-        }
-        return resolve(migrate(path, client, isSync)
-          .then((result) => {
-            done();
-            return result;
-          })
-          .catch((migrateError) => {
-            done();
-            return Promise.reject(migrateError);
-          })
-        );
-      });
-    });
-  }
+function migrate(path, pool, isSync) {
+  assert(pool.query, 'Should have pool.query. Please use pg.Pool.');
+  assert(pool.connect, 'Should have pool.connect. Please use pg.Pool');
   const hooks = [
     '0000-00-00T00:00:00:000Z',
     '9999-99-99T99:99:99:999Z',
@@ -29,13 +13,15 @@ function migrate(path, connection, isSync) {
   const isHook = migration => hooks.some(hook => migration.filename.indexOf(hook) === 0);
 
   const schemaSql = 'INSERT INTO schema_info (version) VALUES ($1)';
-  const executeMigration = migration => Promise.resolve()
-    .then(() => connection.query('BEGIN'))
-    .then(() => connection.query(migration.sql))
-    .then(() => (isHook(migration) ? null : connection.query(schemaSql, [migration.filename])))
-    .then(() => connection.query('COMMIT'))
-    .catch(err => connection.query('ROLLBACK').then(() => Promise.reject(err)))
-    .then(() => migration);
+  const executeMigration = migration => pool.connect().then(client => Promise.resolve()
+    .then(() => client.query('BEGIN'))
+    .then(() => client.query(migration.sql))
+    .then(() => (isHook(migration) ? null : client.query(schemaSql, [migration.filename])))
+    .then(() => client.query('COMMIT'))
+    .catch(err => client.query('ROLLBACK').then(() => Promise.reject(err)))
+    .then(() => client.end())
+    .then(() => migration)
+  );
 
   const endsWith = (str, suffix) => str.indexOf(suffix, str.length - suffix.length) !== -1;
 
@@ -43,7 +29,7 @@ function migrate(path, connection, isSync) {
 
   const filesystemSchemas = fs.readdirSync(path).filter(isValidSchemaFile).sort();
 
-  const getDatabaseSchemas = () => connection.query('SELECT * FROM schema_info')
+  const getDatabaseSchemas = () => pool.query('SELECT * FROM schema_info')
     .then(result => result.rows.map(schema => schema.version));
 
   const filterNewSchemas = (available, existing) =>
@@ -54,14 +40,23 @@ function migrate(path, connection, isSync) {
   const executeMigrations = () => Promise.all([filesystemSchemas, getDatabaseSchemas()])
     .then(promises => filterNewSchemas(promises[0], promises[1]))
     .then(results => Promise.all(results.map(loadSql)))
-    .then(results => Promise.all(results.map(executeMigration)));
+    .then((migrations) => {
+      const results = [];
+      migrations.forEach(task =>
+        results.push((results.slice(-1)[0] || Promise.resolve()).then(() => executeMigration(task)))
+      );
+      return Promise.all(results);
+    });
 
   const schemaTableSql = 'CREATE TABLE IF NOT EXISTS schema_info (version text not null unique)';
 
   return Promise.resolve()
-    .then(isSync ? connection.query(dropSql) : Function)
-    .then(() => connection.query(schemaTableSql))
+    .then(() => (isSync ? pool.query(dropSql) : null))
+    .then(() => pool.query(schemaTableSql))
     .then(executeMigrations)
-    .then(migrations => migrations.map(migration => `Added ${migration.filename} to database.`));
+    .then((migrations) => {
+      pool.end();
+      return migrations.map(migration => `Added ${migration.filename} to database.`);
+    });
 }
 module.exports = migrate;
